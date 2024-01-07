c5a911ae63cacd8ca681eb3b4c1d062ec38cf9d4
Muhammad-fahad007 <Muhammad.fahad0073@gmail.com>
2024-01-08 00:50:31 +0530

    Demo

---
+++ affan.py
@@ -0,0 +1,13 @@
+print("enter m and n and operation, such that m > n")
+
+
+def calculator(m, n, operation):
+    if operation == "+":
+        print(m+n)
+    elif operation == "-":
+        print(m-n)
+    elif operation == "*":
+        print(m*n)
+    elif operation == "/":
+        print(m/n)
+
